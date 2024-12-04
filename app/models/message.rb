99a5a4ba7424496f8acb2353d2f2ca1bcb418d5e
kashyapvad <kashyap.vad@gmail.com>
2024-12-03 20:53:04 -0500

    annotations

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,15 +2,15 @@
 #
 # Table name: messages
 #
-#  id               :bigint           not null, primary key
-#  user_content     :string
-#  response_content :string
-#  user_copy        :string
-#  llm_model        :string
-#  llm_response     :jsonb
-#  inquiry_id       :bigint
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
+#  id                   :bigint           not null, primary key
+#  user_content         :string
+#  user_copy            :string
+#  llm_model            :string
+#  llm_response         :jsonb
+#  llm_response_content :string
+#  inquiry_id           :bigint
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
 #
 class Message < ApplicationRecord
   belongs_to :inquiry
