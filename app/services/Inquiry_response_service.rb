ec490fc189106357da9ee8bba71f1ba6c4d2c581
kashyapvad <kashyap.vad@gmail.com>
2024-12-04 13:20:53 -0500

    fix formatting

--- app/services/Inquiry_response_service.rb
+++ app/services/Inquiry_response_service.rb
@@ -17,8 +17,8 @@ class InquiryResponseService
     return unless public_docs_markdown
     battle_cards = BattleCard.all.map{|b| {topic: b.topic, response: b.response} }.to_s
     prompt = EMAIL_RESPONSE_PROMPT.gsub(INQUIRY_PLACEHOLDER, message.user_content)
-                   .gsub(BATTLECARD_PLACEHOLDER, battle_cards)
-                   .gsub(PUBLIC_DOC_PLACEHOLDER, public_docs_markdown)
+                                  .gsub(BATTLECARD_PLACEHOLDER, battle_cards)
+                                  .gsub(PUBLIC_DOC_PLACEHOLDER, public_docs_markdown)
     response = "#{model}_client".camelize.constantize.send_prompt prompt
     email_response = response[:email_response]
     message.llm_model = model
